e9256709144a3286257db4347b95f5f1399fc436
juan <jcortesj@unal.edu.co>
2022-11-27 23:02:16 -0500

    registro funcionando- aun no insert datos

--- unify_lite/pages/registro.tsx
+++ unify_lite/pages/registro.tsx
@@ -2,18 +2,26 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
-import { FormEvent, useEffect, useState } from 'react'
+import { FormEvent, useCallback, useEffect, useRef, useState } from 'react'
 import InputDef from '../components/inputBonito'
 import styles from '../styles/Home.module.css'
 import mysql from "mysql2/promise"
+import { rejects } from 'assert'
 
 const Registro = () => {
+  
 
     const [nombre, setNombre] = useState("");
     const [clave, setClave] = useState("");
     const [email, setEmail] = useState("");
     const [ig, setIg] = useState("No tiene");
+    
     const [dataUsuarios, setdataAmigos] = useState<any[]>([]);
+    /*refs para borrar la entrada de los inputs */
+    const input1 = useRef<HTMLInputElement>(null);
+    const input2 = useRef<HTMLInputElement>(null);
+    const input3 = useRef<HTMLInputElement>(null);
+    const input4 = useRef<HTMLInputElement>(null);
   
 //primero debemos crear la conexión
 //con la db
@@ -24,7 +32,7 @@ async function getPageData() {
         'ig': ig,
         'email': email
     }
-    console.log(objetoPrueba.toString());
+    console.log(objetoPrueba);
     function getRandomInt(max:number) {
         return Math.floor(Math.random() * max);
       }      
@@ -37,43 +45,78 @@ async function getPageData() {
     const apiUrlEndpoint = './api/insertarUsuario/'+insertarPrueba;
     const response = await fetch(apiUrlEndpoint)
     const res = await response.json();
-    //setdataAmigos(res.datos);
     alert(res.datos);
   } 
 
-
-
-  async function actualizarPagina() {
-    //primero debemos verificar que el usuario no exista en la base de datos
-    let existe = false;
-    if(nombre != ''){
-        const apiUrlEndpoint2 = './api/traerUsuarios';
-    const response = await fetch(apiUrlEndpoint2)
-    const res2 = await response.json();
-    setdataAmigos(res2.usuarios);
-    //buscamos si el dato ya existe
-    dataUsuarios.map(indice => {
-        //traemos los datos
-        let nombreVerificar = indice.apodos;
-        //los guardamos en un array
-        if (nombreVerificar == nombre ) {
-          alert('Este nombre de usuario ya existe, por favor intenta con otro');
-         // window.location.href = '/registro';
-          existe = true;
-        }
-      });
-      if(existe==false){
-        await getPageData();
-        alert('usuario creado, dirigase a login')
-        window.location.href = '/login';
-      }
+  const verificarUsuario =
+    //una promesa es una función que ocurrira eventualmente o nunca
+    async () =>{
+          const apiUrlEndpoint2 = './api/traerUsuarios';
+          const response = await fetch(apiUrlEndpoint2)
+          const res2 = await response.json();
+          setdataAmigos(res2.usuarios);
+          dataUsuarios.map(indice => {
+            //traemos los datos
+            let nombreVerificar = indice.apodos;
+            //los guardamos en un array
+            if (nombreVerificar === nombre) {
+                alert('Nombre de usuario ya en uso')
+                if(input1.current != null){
+                    input1.current.value = '';
+                    setNombre('');
+                }
+                
+                //previene un loop
+            }
+        });
+    }
+  
     
+      
+  
+  
+
+  const actualizarPagina = async (event:any) =>  {
+    event.preventDefault();
+    let existencia = false;
+    //primero debemos verificar que el usuario no exista en la base de datos
+    if(nombre != ''){
+        verificarUsuario();
+       //buscamos si el dato ya existe
+       
+    if(existencia == false){
+        alert('usuario creado');
+    }
+       
     }
     else{
         alert('ingrese un usuario valido');
     }
+    if(input1.current != null){
+        input1.current.value = '';
+        setNombre('');
+    }
+    if(input2.current != null){
+        input2.current.value = '';
+        setClave('');
+    }
+    if(input3.current != null){
+        input3.current.value = '';
+        setEmail('');
+    }
+    if(input4.current != null){
+        input4.current.value = '';
+        setIg('No tiene');
+    }
+
+    
     
   }
+
+  useEffect(()=>{
+    verificarUsuario()
+    
+},[nombre]);
       
 
 
@@ -96,22 +139,22 @@ async function getPageData() {
                         <h1>Ups... esta página no está diseñada para computadores</h1>
                         <h2>Por favor ingresa desde un celular</h2>
                     </div>
-                    <div className={styles.centrado}>
+                    <form  >
                         <div className={styles.divFila}>
                             <h3>¿Cómo quieres llamarte?</h3>
-                            <input name={'nom_user'} required={true} onChange={n => setNombre((n.target as HTMLInputElement).value)} />
+                            <input ref={input1} name={'nom_user'} required={true} onChange={n => setNombre((n.target as HTMLInputElement).value)  }  />
                         </div>
                         <div className={styles.divFila}>
                             <h3>Escribe una contraseña</h3>
-                            <input name={'clave_user'} required={true} type={'password'} onChange={c => setClave((c.target as HTMLInputElement).value)} />
+                            <input ref={input2} name={'clave_user'} required={true} type={'password'} onChange={c => setClave((c.target as HTMLInputElement).value)} />
                         </div>
                         <div className={styles.divFila}>
                             <h3>Escribe tu correo</h3>
-                            <input name={'correo_user'} type={email} required={true} onChange={e => setEmail((e.target as HTMLInputElement).value)} />
+                            <input ref={input3} name={'correo_user'} type={email} required={true} onChange={e => setEmail((e.target as HTMLInputElement).value)} />
                         </div>
                         <div className={styles.divFila}>
                             <h3>Escribe tu ig (Si tienes)</h3>
-                            <input name={'ig_user'} required={true} onChange={i => setIg((i.target as HTMLInputElement).value)} />
+                            <input ref={input4} name={'ig_user'} required={true} onChange={i => setIg((i.target as HTMLInputElement).value)} />
                         </div>
                         <footer className={styles.footerNotMain}>
                             <p> <input type={'checkbox'} /> Acepto los términos y condiciones de uso de la aplicación. </p>  <br></br> <a className={styles.linkNegro} href={"../data/policy"}>
@@ -119,11 +162,11 @@ async function getPageData() {
 
                         </footer>
                         <section className={styles.Flex1}>
-                            <button className={styles.botonEstandar}  onClick={actualizarPagina}>
+                            <button className={styles.botonEstandar} onClick={actualizarPagina} >
                                 ¡Iniciemos! </button>
 
                         </section>
-                    </div>
+                    </form>
 
 
                 </div>
@@ -135,3 +178,7 @@ async function getPageData() {
 }
 
 export default Registro;
+function verificarUsuario() {
+    throw new Error('Function not implemented.')
+}
+
