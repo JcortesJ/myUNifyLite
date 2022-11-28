5767d0b3a6d6bde2268cd6b0f76502b2c5e9ebf0
Jefferson Duvan Ramirez Castañeda <jeffersonduvanrami17@gmail.com>
2022-11-28 10:01:37 -0500

    fff

--- unify_lite/components/notParche.tsx
+++ unify_lite/components/notParche.tsx
@@ -23,15 +23,15 @@ const NotParche= (props:string[]) => {
    <div className={styles2.divAmigo}>
         <img src={"/user_circulo.svg"}className={styles.divAmigoImagen}></img>
         <div className={styles.divAmigoNotInfo}>
-            <h2>{props[0]}</h2> 
-            <h4>Descripcion del grupo: <br></br>{props[1]}</h4>
+            <h2>{props[1]}</h2> 
+            <h4>Descripcion: <br></br>{props[2]}</h4>
             <div className={styles.divEtiqueta}>
               <div className="flex m-0.5 px-1 rounded-md content-center items-center bg-orange text-white">Entretenimiento</div>
             </div>                      
         </div>
         
         <button className = {buttonStyles.buttonTrue} ref = {aceptarBtn}> Aceptar </button>
-        <button className={buttonStyles.buttonFalse} ref={eliminarBtn}> {props[2]}</button>
+        <button className={buttonStyles.buttonFalse} ref={eliminarBtn}> {props[1]}</button>
         
         
    </div>
