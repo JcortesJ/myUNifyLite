019af183fb051a02d93d40a2e9ae66ea59774871
juan <jcortesj@unal.edu.co>
2022-11-28 11:45:16 -0500

    crear evento completamente funcional

---
+++ unify_lite/pages/api/insertarEvento/[insertE].ts
@@ -0,0 +1,25 @@
+
+
+import { kuery } from '../../../config/db';
+
+
+//primero debemos crear la conexión
+//con la db
+export default async function handler(req:any,res:any){
+        try{
+            //intentamos hacer nuestra query
+            const query:string = 'INSERT INTO EVENTO(id_evento,Notificacion_id_notificacion,Creador_id_creador,Lugar_id_lugar,nombre,descripcion,hora,fecha,facultad) VALUES ('+req.query.insertE+')';
+            //
+         
+            const data = await kuery(query,[]);
+            
+            res.status(200).json({datos: data});
+            //terminamos la conexión por seguridad
+            //mostramos en el frontend los valores
+
+        } catch(error:any)
+        {
+            res.status(500).json({error: error.message});
+        }
+
+}
