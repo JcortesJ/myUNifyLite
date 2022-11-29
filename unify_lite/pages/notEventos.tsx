9acb617d4701eb837c5e2f9146680465ffafd037
juan <jcortesj@unal.edu.co>
2022-11-29 11:12:08 -0500

    devjm

--- unify_lite/pages/notEventos.tsx
+++ unify_lite/pages/notEventos.tsx
@@ -9,22 +9,16 @@ import Navbar from '../components/Navbar'
 import styles from '../styles/Login.module.css'
 import { useEffect, useState } from 'react'
 import { useUser } from '../contexts/user'
-const NotAmigos = () => {
+const NotEventos = () => {
   const {user,setUser} = useUser();
-  const [dataAmigos, setdataAmigos] = useState<any[]>(['']);
   const [dataNotificaciones, setNotificaciones] = useState<any[]>([]);
   const [mostrarNotificaciones, setMostrarN] = useState<string[][]>([['Buscando Notificaciones...','cargando info...', 'Borrar', ]]);
   async function getPageData() {
-    console.log('ejecutando')
-    let apiUrlEndpoint = './api/buscarAQuery';
+  
+    let apiUrlEndpoint = './api/traerNot/'+user;
     let response = await fetch(apiUrlEndpoint)
     let res = await response.json();
-    setdataAmigos(res.datos);
-    //traer notificaciones
-     apiUrlEndpoint = './api/buscarSolicitudes';
-     response = await fetch(apiUrlEndpoint)
-     res = await response.json();
-    setNotificaciones(res.solicitudesAmigos);
+    setNotificaciones(res.datos);
   }
 
  
@@ -37,49 +31,35 @@ const NotAmigos = () => {
   );
 
   async function actualizarPagina() {
-    console.log('tocando')
     await getPageData();
     console.log(dataNotificaciones);
     if(dataNotificaciones[0][0] !='' ){
       let arrAux: string[][] = [['jiji']];
     //map para insertar en mostrarAmigos
-    dataAmigos.map(indice => {
+    dataNotificaciones.map(indice => {
       //traemos los datos
-      let solicitante:string = indice.solicitanteID;
-      let solicitado:string = indice.solicitadoID;
+      let nom:string = indice.nombre;
+      //console.log('nombre:' +nom)
       //los guardamos en un array
-      let arrIndice = [solicitante,solicitado];
-      console.log('pase por aqui')
-      if(solicitado ==user){
-      
-        //buscamos el nombre del solicitante
-        dataAmigos.map( (indice) =>{
-          
-            if(indice.ID == arrIndice[0]){
-              arrIndice = [indice.apodos,'quiere ser tu amigo','Eliminar'];
-            }
-        });
-
+      let arrIndice = [nom,'Evento Disponible','Borrar'];
         if (arrAux[0][0] == 'jiji') {
           arrAux[0] = arrIndice;
         }
         else {
           arrAux.push(arrIndice);
         }
-      }
     });
     //luego de hacer el map seteamos la nueva variable
-    if(arrAux[0][0] == 'jiji'){
-      arrAux= [['No tienes notificaciones','nada nuevo por aqui','Borrar']];
-    }
     setMostrarN(arrAux);
-      console.log(dataAmigos);
       //console.log(mostrarAmigos);
       console.log(dataNotificaciones);
       console.log(user);
       console.log(mostrarNotificaciones)
       //ahora buscamos si el usuario tiene notificaciones que nos muestre 
    
+    }
+    else{
+        setMostrarN([['No tienes notificaciones','nada nuevo por aqui','Borrar']])
     }
   }
 
@@ -89,7 +69,7 @@ const NotAmigos = () => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Solicitudes</title>
+        <title>Eventos Nuevos</title>
         <meta name="description" content="Social Media App" />
         <link rel="icon" href="/Frame.ico" />
       </Head>
@@ -97,19 +77,18 @@ const NotAmigos = () => {
       <main className={styles.main}>
       <nav className={styles.divNavSup}>
         
-        <div className={styles.divCurrentVision}>
-        <div onClick={actualizarPagina}>
+        <div className={styles.divNotCurrent}>
+        <Link href={"/notAmigos"}>
          Amigos
-         </div>
+         </Link>
       </div>
         
    
-      <div className={styles.divNotCurrent}>
-      <Link href={"/notEventos"}>
+      <div className={styles.divCurrentVision}>
+      <div onClick={actualizarPagina}>
               Eventos
-              </Link>
+              </div>
           </div>
-        
     </nav>
         <div className={styles.cajitaScroll3}>
         { /*<Notificacion{...["Usuario 1", "Quiere ser tu amix", "Eliminar"]}> </Notificacion> */}
@@ -122,4 +101,4 @@ const NotAmigos = () => {
   )
 }
 
-export default NotAmigos;
+export default NotEventos;
